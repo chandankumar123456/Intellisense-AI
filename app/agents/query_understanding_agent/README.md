e7bade2cf9de48e8b39cee0c94660e9183d32886
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2025-11-30 11:54:11 +0530

    create query understanding schema

---
+++ app/agents/query_understanding_agent/README.md
@@ -0,0 +1,15 @@
+This is Agent 0 in the Agentic RAG Pipeline
+
+# It decides:
+
+. what kind of question the user is asking
+
+. how to rewrite it for retreival
+
+. what retrievers to use (vector/keyword/web/
+YouTube)
+
+. How many chunks to retrieve(top_k params)
+
+. The answer style(consise/detailed/exam-mode, etc.)
+
---
+++ app/agents/query_understanding_agent/schema.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import List, Literal, Annotated
+from pydantic import BaseModel, Field
+
+class Preferences(BaseModel):
+    response_style: str
+    max_length: int
+    domain: str
+
+class QueryUnderstandingInput(BaseModel):
+    user_id: str
+    session_id: str
+    query_text: str
+    conversation_history: List[str] = Field(..., max_items=3)
+    preferences: Preferences
+    timestamp: datetime
+    
+class RetrievalParams(BaseModel):
+    top_k_vector: int
+    top_k_keyword: int
+    top_k_web: int
+    top_k_youtube: int
+    
+class StylePreferences(BaseModel):
+    type: Literal["concise", "detailed", "simple", "exam"]
+    tone: Literal["neutral", "friendly", "technical"]
+
+class QueryUnderstandingOutput(BaseModel):
+    intent: Literal["qa", "explain", "summarize", "compare", "exam", "debug", "none"]
+    rewritten_query: str
+    retrievers_to_use: List[Literal["vector", "keyword", "web", "youtube"]] = Field(..., min_items = 1)
+    retrieval_params: RetrievalParams
+    style_preferences: StylePreferences
+    
